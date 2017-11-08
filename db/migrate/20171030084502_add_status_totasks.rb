914ec76059d7a0e94c05a94f8d52fd3f0bf01b02
HA <swha312@naver.com>
2017-11-08 08:41:20 +0000

    fix

--- db/migrate/20171030084502_add_status_totasks.rb
+++
@@ -1,4 +0,0 @@
-class AddStatusTotasks < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107115039) do
+ActiveRecord::Schema.define(version: 20171108083249) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "content"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
