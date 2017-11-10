2106719e4ea8bc80558d993ea45d16eab6bf187d
HA <swha312@naver.com>
2017-11-10 12:10:16 +0000

    edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :edit, :show, :update]
+  before_action :set_task, only: [:show, :edit, :update, :destroy] 
   
   def create
     @task = current_user.tasks.build(task_params)
@@ -14,6 +15,22 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'タスクは正常に更新されました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクは更新されませんでした。'
+      render :edit
+    end
+  end
+  
+  def show
+  end
+    
   def destroy
     @task.destroy
     flash[:success] = 'タスクを削除しました。'
@@ -21,9 +38,13 @@ class TasksController < ApplicationController
   end
 
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end 
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 
   def correct_user
